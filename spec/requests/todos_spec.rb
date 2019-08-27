eb56f59258da6955012157f7c400697c843c6bfc
Paulina <paulina.recko@gmail.com>
2019-08-27 10:19:26 +0200

    Add test suite for PUT /todos/:id

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -73,4 +73,20 @@ RSpec.describe 'Todos API', type: :request do
        end
      end
   end
+
+  describe 'PUT /todos/:id' do
+    let(:valid_attributes) { { title: 'Shopping' } }
+
+    context 'when the record exists' do
+      before { put "/todos/#{todo_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
 end
