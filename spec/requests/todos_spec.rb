6a9917727d86d63d6fca55e8ff157ffa26273ccc
Paulina <paulina.recko@gmail.com>
2019-08-27 10:06:08 +0200

    Add test suite for POST /todos

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -44,4 +44,33 @@ RSpec.describe 'Todos API', type: :request do
       end
     end
   end
+
+  describe 'POST /todos' do
+    let(:valid_attributes) { { title: 'Learn Elm', created_by: '1' } }
+
+    context 'when the request is valid' do
+      before { post '/todos', params: valid_attributes }
+
+      it 'creates a todo' do
+       expect(json['title']).to eq('Learn Elm')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+       before { post '/todos', params: { title: 'Foobar' } }
+
+       it 'returns status code 422' do
+         expect(response).to have_http_status(422)
+       end
+
+       it 'returns a validation failure message' do
+         expect(response.body)
+           .to match(/Validation failed: Created by can't be blank/)
+       end
+     end
+  end
 end
