fdff9eeaf5d71dfad71cdaae3c14869e023a5e56
Paulina <paulina.recko@gmail.com>
2019-09-01 18:47:34 +0200

    Add test suite for GET /todos/:todo_id/items

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -5,4 +5,30 @@ RSpec.describe 'Items API' do
   let!(:items) { create_list(:item, 20, todo_id: todo.id) }
   let(:todo_id) { todo.id }
   let(:id) { items.first.id }
+
+  describe 'GET /todos/:todo_id/items' do
+    before { get "/todos/#{todo_id}/items" }
+
+    context 'when todo exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all todo items' do
+        expect(json.size).to eq(20)
+      end
+    end
+
+    context 'when todo does not exist' do
+      let(:todo_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Todo/)
+      end
+    end
+  end
 end
