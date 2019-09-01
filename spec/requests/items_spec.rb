44f9e296946796174bd9610102ef9f822cb164a8
Paulina <paulina.recko@gmail.com>
2019-09-01 19:01:17 +0200

    Add test suite for DELETE /todos/:id

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -110,4 +110,12 @@ RSpec.describe 'Items API' do
       end
     end
   end
+
+  describe 'DELETE /todos/:id' do
+    before { delete "/todos/#{todo_id}/items/#{id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
 end
