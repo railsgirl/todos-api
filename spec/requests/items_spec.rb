2f07c9dc6c9996796fede846fad1d5930a3e78aa
Paulina <paulina.recko@gmail.com>
2019-09-01 18:43:45 +0200

    Initialize test data

---
+++ spec/requests/items_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'Items API' do
+  let!(:todo) { create(:todo) }
+  let!(:items) { create_list(:item, 20, todo_id: todo.id) }
+  let(:todo_id) { todo.id }
+  let(:id) { items.first.id }
+end
